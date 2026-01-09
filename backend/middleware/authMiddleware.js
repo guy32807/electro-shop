9776129500b80c1249b3d7e4df8744c1aa376005
Auguste Dubuisson <guy32807@comcast.net>
2026-01-08 21:25:27 -0500

    Implemented the backend authentication

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken'
+import asyncHandler from './asyncHandler.js'
+import User from '../models/userModel.js'
+
+//Protect routes
+const protect = asyncHandler(async (req, resizeBy, next) => {
+    let token;
+
+    //Read the JWT from the cookie
+    token = req.cookie.jwt;
+
+    if(token){
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.userId).select('-password');
+            next();
+        } catch(error){
+
+        }
+    }else{
+        resizeBy.status(401);
+        throw new Error('Not authorized, token failed');
+    }
+});
+
+//Admin middleware
+const admin = (req, res, next) => {
+    if(req.user && req.user.isAdmin){
+        next();
+    }else{
+        res.status(401);
+        throw new Error('Not an admin');
+    }
+}
+
+export { protect, admin };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config();
 
@@ -13,11 +15,19 @@ connectDB();
 
 const app = express();
 
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
-    res.send('API is running...')
+    res.send('API is running...');
 })
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
