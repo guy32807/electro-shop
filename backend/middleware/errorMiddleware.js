382a9d81df518fc20b32a576392f923211bfe85d
Auguste Dubuisson <guy32807@comcast.net>
2026-01-09 16:22:58 -0500

    Implemented the frontend authentication and registration screens.

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,15 +8,14 @@ const errorHandler = (err, req, res, next) => {
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = err.message;
 
-    if(err.name  === 'CastError' && err.kink === 'ObjectId'){
+    if(err.name  === 'CastError' && err.kind === 'ObjectId'){
         message = `Resource not found`;
         statusCode = 404;
-
-        res.status(statusCode).json({
+    }
+    res.status(statusCode).json({
             message,
             stack: process.env.NODE_ENV === 'production' ? '' : err.stack
         })
-    }
 }
 
 export {notFound, errorHandler};
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,30 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { updateCart } from '../utils/cartUtils'
+import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
-const initialState = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : { cartItems: [] }
+const initialState = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-        addToCart: (state, action) => {
-            const item = action.payload
-            const existingItem = state.cartItems.find(cartItem => cartItem._id === item._id)
-            if (existingItem) {
-                state.cartItems = state.cartItems.map(cartItem => cartItem._id === existingItem._id ? item : cartItem)
-            } else {
-                state.cartItems = [...state.cartItems, item]
-            }
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existingItem = state.cartItems.find(
+        (cartItem) => cartItem._id === item._id
+      );
+      if (existingItem) {
+        state.cartItems = state.cartItems.map((cartItem) =>
+          cartItem._id === existingItem._id ? item : cartItem
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
 
-            return updateCart(state);
-        },
-        removeFromCart: (state, action) => {
-            state.cartItems = state.cartItems.filter((x) => x._id !== action.payload)
+      return updateCart(state);
+    },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
 
-            return updateCart(state);
-        }
+      return updateCart(state);
+    },
+    saveShippingAddress: (state, action) => {
+        state.shippingAddress = action.payload;
+        return updateCart;
     }
-})
+  },
+});
 
-export const { addToCart, removeFromCart } = cartSlice.actions
-export default cartSlice.reducer
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
