9fdc9b40d0483535f2e5e7d5196288a297b0194d
Auguste Dubuisson <guy32807@comcast.net>
2026-01-09 18:33:34 -0500

    Implemented the shipping address and payment screen.

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -31,10 +31,14 @@ const cartSlice = createSlice({
     },
     saveShippingAddress: (state, action) => {
         state.shippingAddress = action.payload;
-        return updateCart;
+        updateCart(state);
+    },
+    savePaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload;
+      updateCart(state);
     }
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress, savePaymentMethod } = cartSlice.actions;
 export default cartSlice.reducer;
